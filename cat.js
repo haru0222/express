2a170a5fcb8e1fe974946b9c6102ce95bb85fd1d
haru0222 <banbenliaoshi448@gmail.com>
2025-07-23 09:54:47 +0900

    Create cat.js

---
+++ cat.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+const request = require('request');
+const cors = reqire('cors');//corsミドルウェアを追加
+
+//corsミドルウェアを使用
+//router.use(cors());
+
+router.get('/', async (req, res) => {
+  request('https://api.thecatapi.com/v1/images/serch', function (error, response, body) {{
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.json(data);
+    }
+  });
+})
+
+module.express = router;
